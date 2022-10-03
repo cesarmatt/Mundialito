59c1d35598c20e1d6d277e9ee42bafd4eb1ab868
Matheus Cesar <matheuscesar@MacBook-Air-de-Matheus.local>
2022-10-03 19:48:47 -0300

    Basic UI structure, but not creating session yet

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mundialito/app/modules/mundialito/create/create_mundialito_module.dart';
 import 'package:mundialito/app/modules/mundialito/tournament/tournament_module.dart';
 import 'package:mundialito/app/modules/onboarding/onboarding_module.dart';
+import 'package:mundialito/app/modules/onboarding/submodules/login/login_module.dart';
 
 class AppModule extends Module {
 
@@ -11,6 +12,7 @@ class AppModule extends Module {
   @override
   final List<ModularRoute> routes = [
     ModuleRoute(Modular.initialRoute, module: OnboardingModule()),
+    ModuleRoute('/login', module: LoginModule()),
     ModuleRoute('/mundialito', module: CreateMundialitoModule()),
     ModuleRoute('/tournament', module: TournamentModule())
   ];

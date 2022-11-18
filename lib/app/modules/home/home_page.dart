5359522a58e72c7541bc1c638e7c11047ea69865
Matheus Cesar <matheuscesar@MacBook-Air-de-Matheus.local>
2022-11-17 22:15:31 -0300

    Floating action button for join and create mundialito created

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -5,6 +5,8 @@ import 'package:mundialito/app/modules/home/home_view_model.dart';
 import 'package:mundialito/app/modules/home/list/mundialito_list_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mundialito/app/modules/home/widgets/floatingactionbutton/action_button_widget.dart';
+import 'package:mundialito/app/modules/home/widgets/floatingactionbutton/home_expandable_floating_action_button_widget.dart';
 import 'package:mundialito/app/shared/widgets/liststate/empty_state_widget.dart';
 import 'package:mundialito/app/shared/widgets/liststate/primary_loader_widget.dart';
 
@@ -27,25 +29,36 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { _onCreatePressed(); },
-        child: SvgPicture.asset('assets/flag_circle_black_24dp.svg',
-            color: Colors.white, width: 40, height: 40),
-      ),
-      body: Observer(builder: (_) {
-        if (_viewModel.isLoading) {
-          return const PrimaryLoaderWidget();
-        } else if (_viewModel.mundialitos.isEmpty) {
-          return const EmptyStateWidget(message: "You don't have any mundialitos yet.",);
-        } else {
-          return SingleChildScrollView(
-            child: MundialitoListWidget(
-                onItemPressed: (mundialitoId) { _onMundialitoPressed(mundialitoId); },
-                mundialitos: _viewModel.mundialitos
-            ),
-          );
-        }
-      })
+        floatingActionButton: HomeExpandableFloatingActionButtonWidget(
+            distance: 90,
+            children: [
+              ActionButton(
+                icon: const Icon(Icons.add),
+                onPressed: _onCreatePressed,
+              ),
+              ActionButton(
+                icon: SvgPicture.asset('assets/flag_circle_black_24dp.svg', color: Colors.white, width: 40, height: 40),
+                onPressed: _onJoinPressed,
+              ),
+            ]
+        ),
+        body: Observer(builder: (_) {
+          if (_viewModel.isLoading) {
+            return const PrimaryLoaderWidget();
+          } else if (_viewModel.mundialitos.isEmpty) {
+            return const EmptyStateWidget(
+              message: "You don't have any mundialitos yet.",);
+          } else {
+            return SingleChildScrollView(
+              child: MundialitoListWidget(
+                  onItemPressed: (mundialitoId) {
+                    _onMundialitoPressed(mundialitoId);
+                  },
+                  mundialitos: _viewModel.mundialitos
+              ),
+            );
+          }
+        })
     );
   }
 
@@ -57,5 +70,9 @@ class HomePageState extends State<HomePage> {
   Future<void> _onCreatePressed() async {
     await Modular.to.pushNamed('/mundialito/');
     await _viewModel.getMundialitos();
+  }
+
+  Future<void> _onJoinPressed() async {
+    print("Join pressed!");
   }
 }
---
+++ lib/app/modules/home/widgets/floatingactionbutton/home_expandable_floating_action_button_widget.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mundialito/app/modules/home/widgets/floatingactionbutton/expanding_action_button_widget.dart';
+
+class HomeExpandableFloatingActionButtonWidget extends StatefulWidget {
+  const HomeExpandableFloatingActionButtonWidget(
+      {super.key,
+      this.initialOpen,
+      required this.distance,
+      required this.children});
+
+  final bool? initialOpen;
+  final double distance;
+  final List<Widget> children;
+
+  State<HomeExpandableFloatingActionButtonWidget> createState() =>
+      _HomeExpandableFloatingActionButtonState();
+}
+
+class _HomeExpandableFloatingActionButtonState
+    extends State<HomeExpandableFloatingActionButtonWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastOutSlowIn,
+        reverseCurve: Curves.easeOutQuad,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggle() {
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab(),
+          ..._buildExpandingActionButtons(),
+          _buildTapToOpenFab(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTapToCloseFab() {
+    return SizedBox(
+      width: 56.0,
+      height: 56.0,
+      child: Center(
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4.0,
+          child: InkWell(
+            onTap: _toggle,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.close,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        )
+      ),
+    );
+  }
+
+  List<Widget> _buildExpandingActionButtons() {
+    final children = <Widget>[];
+    final count = widget.children.length;
+    final step = 90.0 / (count - 1);
+    for (var i = 0, angleInDegrees = 0.0; i < count; i++, angleInDegrees += step) {
+      children.add(
+        ExpandingActionButtonWidget(
+          directionInDegrees: angleInDegrees,
+          maxDistance: widget.distance,
+          progress: _expandAnimation,
+          child: widget.children[i],
+        ),
+      );
+    }
+    return children;
+  }
+
+  Widget _buildTapToOpenFab() {
+    return IgnorePointer(
+      ignoring: _open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        transform: Matrix4.diagonal3Values(
+          _open ? 0.7 : 1.0,
+          _open ? 0.7 : 1.0,
+          1.0,
+        ),
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: _open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: FloatingActionButton(
+            onPressed: _toggle,
+            child: const Icon(Icons.create),
+          ),
+        ),
+      ),
+    );
+  }
+}
